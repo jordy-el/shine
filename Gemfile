7c0ebf169d0f82a4f43924176175bb39adf3dbca
Jordan Ellis-Lynch <jordan_nose2@hotmail.com>
2017-09-14 01:08:11 +1000

    Add efficient database search

--- Gemfile
+++ Gemfile
@@ -48,3 +48,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'webpacker'
 gem 'foreman'
 gem 'devise'
+gem 'faker'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Customer.all.find_each(&:destroy)
+
+350000.times do |i|
+  Customer.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, username: "#{Faker::Internet.user_name}#{i}", email: "#{Faker::Internet.user_name}#{i}@#{Faker::Internet.domain_name}")
+  print '.' if i % 1000 == 0
+end
+puts
