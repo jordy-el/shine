4c23d1ead6df8905c00bf914553ca549c069864e
Jordan Ellis-Lynch <jordan_nose2@hotmail.com>
2017-09-16 22:07:22 +1000

    Move template from index.ts to separate html

--- app/javascript/CustomerSearchComponent/index.ts
+++ app/javascript/CustomerSearchComponent/index.ts
@@ -1,31 +1,10 @@
 import { Component } from '@angular/core';
 import { Http } from '@angular/http';
+import template from './template.html';
 
 const CustomerSearchComponent = Component({
     selector: 'shine-customer-search',
-    template: `
-    <header>
-        <h1 class="h2">Customer Search</h1>
-    </header>
-    <section class="search-form">
-        <form>
-            <label for="keywords" class="sr-only">Keywords</label>
-            <input type="text" id="keywords" name="keywords" placeholder="First Name, Last Name, or Email Address" class="form-control input-lg" bindon-ngModel="keywords" on-ngModelChange="search()">
-        </form>
-    </section>
-    <section class="search-results" *ngIf="customers">
-        <header>
-            <h1 class="h3">Results</h1>
-        </header>
-        <ol class="list-group">
-            <li *ngFor="let c of customers" class="list-group-item">
-                <h3 class="pull-right"><small class="text-uppercase">Joined </small>{{ c.created_at }}</h3>
-                <h2 class="h3">{{ c.first_name + ' ' + c.last_name }}<small> {{ c.username }}</small></h2>
-                <h4>{{ c.email }}</h4>
-            </li>
-        </ol>
-    </section>
-    `
+    template
 }).Class({
     constructor: [
         Http,
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,17 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker');
 
-module.exports = environment
+environment.loaders.set('html', {
+  test: /\.html$/,
+  use: [{
+    loader: 'html-loader',
+    options: {
+      minimize: true,
+      removeAttributeQuotes: false,
+      caseSensitive: true,
+      customAttrSurround: [ [/#/, /(?:)/], [/\*/, /(?:)/], [/\[?\(?/, /(?:)/] ],
+      customAttrAssign: [ /\)?\]?=/ ]
+    }
+  }]
+});
+
+module.exports = environment;
